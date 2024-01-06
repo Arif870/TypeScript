367f81b7f6b2d2b88c0e200ffe7555397822d175
Arif uz zaman <arifuzzaman959@gmail.com>
2024-01-06 14:36:29 +0600

    arry and tuples

---
+++ src/array_tuples.ts
@@ -0,0 +1,9 @@
+// String type array
+const students: string[] = ["Arif", "ashik", "nasrin"];
+
+// Number type array
+const roll_Number: Number[] = [10, 20, 30];
+
+// Array Tuples
+
+const tuples: [string, boolean, number] = ["Arif", true, 10];
